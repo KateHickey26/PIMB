202a2af2644c7790b9d1c53f83beca064108f206
kate hickey <kate.hickey26@gmail.com>
2020-03-11 14:58:49 +0000

    edits

---
+++ IT_Group_Project/urls.py
@@ -0,0 +1,33 @@
+"""IT_Group_Project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from inspeakers import views
+<<<<<<< HEAD
+from django.conf import settings
+from django.conf.urls.static import static
+=======
+>>>>>>> 502c947368b00c435cb46a839dcf0134ea4faca7
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('inspeakers/', include('inspeakers.urls')),
+<<<<<<< HEAD
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+=======
+]
+>>>>>>> 502c947368b00c435cb46a839dcf0134ea4faca7
