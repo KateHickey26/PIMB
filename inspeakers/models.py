7bafcbef8d93c709ed211a0cf8821e3a7267fc37
kate hickey <kate.hickey26@gmail.com>
2020-03-11 14:48:04 +0000

    updates

--- inspeakers/models.py
+++ inspeakers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    # Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # The additional attributes we wish to include.
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class SpeakerProfile(models.Model):
+    speaker = models.OneToOneField(Speaker, on_delete=models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.PhoneField(blank=True)
+
+    def __str__(self):
+        return self.speaker.username
---
+++ inspeakers/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from inspeakers import views
+
+# app_name = ' '
+
+# the urls below will follow the app name above
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('home/speakerprofile/', views.speakerprofile, name='speaker_profile'),
+    path('signup/', views.sign_up, name='sign_up'),
+    path('login/myaccount/', views.my_account, name='my_account'),
+    # if we decide to have a new page to add a review
+    path('home/speakerprofile/addreview/', views.add_review, name='add_review'),
+]
