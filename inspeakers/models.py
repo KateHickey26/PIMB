231ca235994b2398ef833e8e41e842008eb5f70d
kate hickey <kate.hickey26@gmail.com>
2020-03-20 17:08:35 +0000

    adding comment box functionality

--- inspeakers/models.py
+++ inspeakers/models.py
@@ -23,6 +23,20 @@ class SpeakerProfile(models.Model):
     def __str__(self):
         return self.speaker.username
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False) #to prevent spam, and manually allow comments, set to false
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     popularity = models.IntegerField(default=0)
