6fe9958b489fc75a117ab23c3b1a81932f7463cf
Liam Krupej <liam.krupej@student.gla.ac.uk>
2020-03-30 12:37:41 +0100

    footer stuff

--- inspeakers/urls.py
+++ inspeakers/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 from inspeakers import views
 
 app_name = 'inspeakers'
@@ -12,6 +13,7 @@ urlpatterns = [
     path('home/fav/', views.mfav, name='fav'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
     path('home/speakerprofile/<slug:speaker_profile_slug>', views.speakerprofile, name='speaker_profile'),
     path('home/speakerprofile/<slug:speaker_profile_slug>/comment', views.comment, name='comment'),
     path('home/speakerprofile', views.speakerprofileedit, name='speaker_profile_edit'),
@@ -20,4 +22,5 @@ urlpatterns = [
     path('myaccount/', views.my_account, name='my_account'),
     path('myfavourite/', views.my_favourite, name='my_favourite'),
     path('myreviews/', views.my_reviews, name='my_reviews'),
+
 ]
