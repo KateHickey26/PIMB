5f58fbb462101e987c451b0c7aa5563273678e4a
LLehtola <56760014+LLehtola@users.noreply.github.com>
2020-03-30 20:35:37 +0100

    Added first tests

--- inspeakers/tests.py
+++ inspeakers/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client, override_settings
+from django.urls import reverse
 
 # Create your tests here.
+
+@override_settings(SECURE_SSL_REDIRECT=False)
+class InspeakersTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    # Test about page
+    def test_about(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed('inspeakers/about.html')
+
+    # Test that the login page redirects to home page after logging in
+    def test_redirect_after_login(self):
+        response = self.client.post(reverse('inspeakers:login'), {"username": "jsmith", "password": "jsmith123"})
+        self.assertRedirects(response, reverse('inspeakers:home'))
+
+
