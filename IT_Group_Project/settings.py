d9b184e05dd86d3bc96da67d1079fd46d66f426b
Liam Krupej <liam.krupej@student.gla.ac.uk>
2020-03-30 22:51:31 +0100

    social updates

--- IT_Group_Project/settings.py
+++ IT_Group_Project/settings.py
@@ -156,7 +156,7 @@ REGISTRATION_OPEN = True
 # If True, the user will be automatically logged in after registering.
 REGISTRATION_AUTO_LOGIN = True
 # The URL that Django redirects users to after logging in.
-LOGIN_REDIRECT_URL = 'rango:index'
+LOGIN_REDIRECT_URL = 'inspeakers:home'
 # The page users are directed to if they are not logged in.
 # This was set in a previous chapter. The registration package uses this, too.
 LOGIN_URL = 'auth_login'
